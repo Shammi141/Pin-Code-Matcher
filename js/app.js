16eead4e0d470d2570b706c0fc4c4f081cb2c715
Shammi141 <jannatulshammi181@gmail.com>
2022-08-21 00:31:43 +0600

    done pin matching

--- js/app.js
+++ js/app.js
@@ -24,3 +24,48 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const displayPinField = document.getElementById('disply-pin');
     displayPinField.value = pin;
 });
+
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    //console.log(event.target.innerText)
+    const number = event.target.innerText;  //target diye vitorer element paoa jay
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        //console.log(number);
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else if(number === '<'){
+            const digits = previousTypedNumber.split('');   // split() doese array of string 
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number
+        typedNumberField.value = newTypedNumber;
+    }
+});
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('disply-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccessMsg = document.getElementById('pin-success');
+    const pinFailureMsg = document.getElementById('pin-failure');
+    if(typedNumber === currentPin){
+        //const pinSuccessMsg = document.getElementById('pin-success');
+        pinSuccessMsg.style.display = 'block';  //block will show, none will hide
+        pinFailureMsg.style.display = 'none';
+    }
+    else{
+        // const pinFailureMsg = document.getElementById('pin-failure');
+        pinFailureMsg.style.display = 'block';
+        pinSuccessMsg.style.display = 'none';
+    }
+});
